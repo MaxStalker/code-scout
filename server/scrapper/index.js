f33137e0cace34563e3045f499764be8dde920e0
Max Daunarovich <max.daunarovich@gmail.com>
2022-09-09 14:51:00 +0300

    Remove server code

--- server/scrapper/index.js
+++
@@ -1,4 +0,0 @@
-require = require("esm")(module /*, options*/);
-
-let [filename] = process.argv.slice(2);
-require(`./${filename}`)
--- server/scrapper/process-contract-deployed.js
+++
@@ -1,7 +0,0 @@
-import {readDB} from "./db";
-
-(async ()=>{
-	const data = readDB("./data/events-contract-deployed.json");
-	const filteredCollectible = data.filter(contract => contract.name === "Collectible")
-	console.log(filteredCollectible.length)
-})()
