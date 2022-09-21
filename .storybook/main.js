cbaf9ce7b6e213fa72e6ae6dc809d900f1bdf859
Max Daunarovich <max.daunarovich@gmail.com>
2022-09-21 16:33:12 +0300

    Fix  storybook setup, pair it with tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,19 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   "framework": "@storybook/react",
   "core": {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+// .storybook/preview.js
+import '../styles/globals.css';
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +16,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  previewTabs: {
+    'storybook/docs/panel': { index: -1 },
   },
 }
